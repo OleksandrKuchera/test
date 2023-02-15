b04bfc40bbed207643e5ef98d7c9d41b4abe328c
gumottt <116847212+gumottt@users.noreply.github.com>
2023-02-15 13:14:02 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+const fs = require('fs'); 
+
+// function test(id1,name1, arr){
+//     fs.readFile('./test.csv', 'utf-8', (err, data) => {
+//         fs.writeFile('./test.csv', arr.  ,() =>{})
+//     })
+// }
+
+// test('d','a')
+
+
+// }
+// fs.readFile('./test.csv', 'utf-8', (err, data) => {
+//     fs.writeFile('./test.csv','', () =>{})
+// })
+const array = [
+   { id:'',
+    name:''}
+];
+
+function test(id1, name1,arr){
+arr.forEach(element => {
+    arr.push(id= 'id1')
+    arr.push(name= 'name1')
+    
+});
+        // arr.push(id= 'id1')
+        // arr.push(name= 'name1')
+    }
+
+test('a','f',array)
+test('a','f',array)
+fs.writeFileSync('./test.csv', JSON.stringify(array), 'utf-8');
